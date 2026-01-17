59d55724ee87271ac705a57230a78ea8b144eecd
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2026-01-17 04:56:34 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,6 +3,4 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-// Дебаг-информация
-console.log("Mini App загружена!");
-console.log("Тема: " + (tg.colorScheme || "не определена"));
+console.log("Mini App загружено!");
