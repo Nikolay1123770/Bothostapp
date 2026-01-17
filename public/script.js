487aa95a7c4e94f9c534fdc0d86810061b0cf557
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2026-01-17 04:12:17 +0300

    Delete public directory

--- public/script.js
+++
@@ -1,8 +0,0 @@
-// Инициализация Telegram Mini App
-const tg = window.Telegram.WebApp;
-tg.ready();
-tg.expand();
-
-// Дебаг-информация
-console.log("Mini App загружена!");
-console.log("Тема: " + (tg.colorScheme || "не определена"));
